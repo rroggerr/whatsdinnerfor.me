fbb89af7fa70768cf76624c8d88787d1213a4735
Roger Geng <geng.roger@gmail.com>
2021-06-05 20:14:49 -0400

    Noapi-test

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,23 +4,25 @@ import {YelpApi} from './yelpApi'
 require('dotenv').config()
 
 const PORT = process.env.PORT || 8080;
-const yelpApiKey = process.env.YELP_API_KEY || ''
+// const yelpApiKey = process.env.YELP_API_KEY || ''
 
 const app = express();
-const YelpClient = new YelpApi(yelpApiKey)
+// const YelpClient = new YelpApi(yelpApiKey)
 
 app.use('/build', express.static('build'));
 app.use(express.static('public'));
 
-app.get('/api/restaurants', async (req, res, next) => {
-  try {
-    const yelpResp = await YelpClient.getYelpPlaces();
-    res.json(yelpResp);
-  } catch (err) {
-    next(err);
-  }
-  console.log(req.connection.remoteAddress)
-})
+console.log(process.env.YELP_API_KEY)
+
+// app.get('/api/restaurants', async (req, res, next) => {
+//   try {
+//     const yelpResp = await YelpClient.getYelpPlaces();
+//     res.json(yelpResp);
+//   } catch (err) {
+//     next(err);
+//   }
+//   console.log(req.connection.remoteAddress)
+// })
 
 app.listen(PORT, function () {
     console.log(`App is listening on port ${PORT}!`);
